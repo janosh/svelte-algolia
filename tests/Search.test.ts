2077096a02aa10c60366d353db8f12009c11df2c
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-02-27 20:42:14 +0000

    add test Search focuses input on icon click

--- tests/Search.test.ts
+++ tests/Search.test.ts
@@ -1,4 +1,5 @@
 import { readFileSync } from 'fs'
+import { tick } from 'svelte'
 import { expect, test } from 'vitest'
 import PokemonHit from '../src/components/PokemonHit.svelte'
 import Search from '../src/lib/Search.svelte'
@@ -7,6 +8,7 @@ const appId = import.meta.env.VITE_ALGOLIA_APP_ID as string
 const searchKey = import.meta.env.VITE_ALGOLIA_SEARCH_KEY as string
 
 const req_props = { appId, searchKey, indices: { Pokedex: PokemonHit } }
+const opt_props = { placeholder: `Search Pokedex`, ariaLabel: `Search` }
 
 test(`readme documents all props`, () => {
   const readme = readFileSync(`readme.md`, `utf8`)
@@ -19,4 +21,24 @@ test(`readme documents all props`, () => {
   for (const prop of Object.keys(instance.$$.props)) {
     expect(readme).to.contain(`\n| \`${prop}\``)
   }
+})
+
+test(`Search focuses input on icon click`, async () => {
+  const instance = new Search({
+    target: document.body,
+    props: { ...req_props, ...opt_props },
+  })
+
+  expect(instance).toBeTruthy()
+
+  expect(document.querySelector(`input.hasFocus`)).toBeFalsy()
+
+  const btn = document.querySelector(`button[title='${opt_props.ariaLabel}']`)
+
+  expect(btn).toBeTruthy()
+
+  btn?.click()
+
+  await tick()
+  expect(document.querySelector(`input.hasFocus`)).toBeTruthy()
 })
